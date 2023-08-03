36bd002ac0de1afb7cf89444a7e8ee134eddb3a5
Victor Netto <victorhmnetto@gmail.com>
2023-08-03 17:43:02 +0200

    feat: initilize root_ca struct with the cert/key from main
    The root CA certificate is now initilized only one time in the code.

--- src/cert/cert.c
+++ src/cert/cert.c
@@ -1,17 +1,12 @@
-// Copyright (c) 2017, 2018, 2019 Linus Karlsson
-// Copyright (c) 2023 Victor Netto
-
-#include <openssl/obj_mac.h>
-#include <openssl/x509.h>
-#include <stdint.h>
-#include <stdio.h>
+#include "cert.h"
 
 #include <openssl/conf.h>
-#include <openssl/engine.h>
-#include <openssl/err.h>
+#include <openssl/obj_mac.h>
 #include <openssl/pem.h>
 #include <openssl/rand.h>
+#include <openssl/x509.h>
 #include <openssl/x509v3.h>
+#include <stdio.h>
 
 #define RSA_KEY_BITS (4096)
 
@@ -20,47 +15,63 @@
 #define DN_LOCALITY "Nancy"
 #define DN_ORGANIZATION "LORIA"
 #define DN_ORGANIZATION_UNIT "RESIST"
-#define DN_COMMON_NAME "SNI Changer"
 
-#define PRIVATE_PASSWORD "xarope12"
-
-int load_root_ca_key_and_crt(const char *root_ca_key_path,
-                             EVP_PKEY **root_ca_key,
-                             const char *root_ca_crt_path, X509 **root_ca_crt) {
+/**
+ * Load root cert and root key to sign the generated certificates.
+ *
+ * @param root_ca -> Struct containing the location of the cert/key.
+ * @param key_location -> Location of the key.
+ * @param cert_location -> Location of the certificate.
+ * @param key_password -> Key password.
+ * @return -> 0 if success, -1 otherwise.
+ */
+int load_root_ca_key_and_crt(struct root_ca *root_ca, char *key_location,
+                             char *cert_location, char *key_password) {
+    root_ca->root_ca_key = NULL;
+    root_ca->root_ca_crt = NULL;
     BIO *bio = NULL;
 
-    // Load certificate
+    // Load certificate.
     bio = BIO_new(BIO_s_file());
-    if (!BIO_read_filename(bio, root_ca_crt_path))
+    if (!BIO_read_filename(bio, cert_location))
         goto error;
 
-    *root_ca_crt = PEM_read_bio_X509(bio, NULL, NULL, NULL);
-    if (!*root_ca_crt)
+    root_ca->root_ca_crt = PEM_read_bio_X509(bio, NULL, NULL, NULL);
+    if (!root_ca->root_ca_crt)
         goto error;
 
     BIO_free_all(bio);
 
-    // Load private key
+    // Load private key.
     bio = BIO_new(BIO_s_file());
-    if (!BIO_read_filename(bio, root_ca_key_path))
+    if (!BIO_read_filename(bio, key_location))
         goto error;
 
-    *root_ca_key = PEM_read_bio_PrivateKey(bio, NULL, NULL, PRIVATE_PASSWORD);
-    if (!root_ca_key)
-        goto error;
+    root_ca->root_ca_key =
+        PEM_read_bio_PrivateKey(bio, NULL, NULL, key_password);
 
     BIO_free_all(bio);
 
-    return 1;
+    return 0;
 error:
     // Free everything in case of error.
     BIO_free_all(bio);
-    X509_free(*root_ca_crt);
-    EVP_PKEY_free(*root_ca_key);
-    return 0;
+    X509_free(root_ca->root_ca_crt);
+    EVP_PKEY_free(root_ca->root_ca_key);
+    return -1;
 }
 
-int generate_certificate_key(EVP_PKEY **key, X509_REQ **req, char *hostname) {
+/**
+ * Generate the key intended for use with the destination website. Once the key
+ * is created, it sign itself to demonstrate possession of the private key.
+ *
+ * @param key -> Created key.
+ * @param req -> Created signature request.
+ * @param hostname -> Hostname (ex: www.example.com) to be used as Common Name.
+ * @return -> 0 if success, -1 otherwise.
+ */
+int generate_certificate_key(EVP_PKEY **key, X509_REQ **req,
+                             const char *hostname) {
     // Create key
     *key = EVP_PKEY_new();
     if (!*key)
@@ -101,21 +112,27 @@ int generate_certificate_key(EVP_PKEY **key, X509_REQ **req, char *hostname) {
     if (!X509_REQ_sign(*req, *key, EVP_sha256()))
         goto error;
 
-    return 1;
+    return 0;
 error:
     // Free everything if it is not needed.
     EVP_PKEY_free(*key);
     X509_REQ_free(*req);
-    return 0;
+    return -1;
 }
 
+/**
+ * Create random serial to be used by the recently created certificate.
+ *
+ * @param crt -> New certificate.
+ * @return -> 0 if success, -1 otherwise.
+ */
 int generate_random_serial(X509 *crt) {
     unsigned char serial_bytes[20];
 
     if (RAND_bytes(serial_bytes, sizeof(serial_bytes)) != 1) {
         fprintf(stderr, "(error) Failed to create random sequence os bytes!\n");
 
-        return 0;
+        return -1;
     }
 
     // Transform into number and then to ASN1, to be used as a serial number.
@@ -134,16 +151,28 @@ int generate_random_serial(X509 *crt) {
     ASN1_INTEGER_free(asn_serial);
     BN_free(bn_serial);
 
-    return 1;
+    return 0;
 }
 
-int sign_certificate(EVP_PKEY *ca_key, X509 *ca_crt, EVP_PKEY **key, X509 **crt,
-                     char *hostname) {
+/**
+ * Sign the certificate generated for the specific website. A hostname is passed
+ * as parameter to assign the subject_alt_name, which must contain the website
+ * for browsers to recognize the authenticity of the certificate.
+ *
+ * @param ca_key -> Root key.
+ * @param ca_crt -> Root certificate.
+ * @param key -> New key.
+ * @param crt -> New certificate.
+ * @param hostname -> Domain (ex: www.example.com).
+ * @return -> 0 if success, -1 otherwise.
+ */
+int sign_certificate(EVP_PKEY *ca_key, const X509 *ca_crt, EVP_PKEY **key,
+                     X509 **crt, const char *hostname) {
 
     // Create private key and request CSR.
     X509_REQ *req = NULL;
-    if (!generate_certificate_key(key, &req, hostname))
-        return 0;
+    if (generate_certificate_key(key, &req, hostname) == -1)
+        return -1;
 
     *crt = X509_new();
     if (!*crt)
@@ -160,14 +189,14 @@ int sign_certificate(EVP_PKEY *ca_key, X509 *ca_crt, EVP_PKEY **key, X509 **crt,
     EVP_PKEY *req_pubkey = X509_REQ_get_pubkey(req);
     X509_set_pubkey(*crt, req_pubkey);
 
-    if (!generate_random_serial(*crt))
+    if (generate_random_serial(*crt) == -1)
         goto error;
 
     // Set the SAN (subjectNameAlt) to the domain.
     GENERAL_NAMES *gens = sk_GENERAL_NAME_new_null();
     GENERAL_NAME *gen = GENERAL_NAME_new();
     ASN1_IA5STRING *ia5 = ASN1_IA5STRING_new();
-    ASN1_STRING_set(ia5, hostname, strlen(hostname));
+    ASN1_STRING_set(ia5, hostname, (int) strlen(hostname));
     GENERAL_NAME_set0_value(gen, GEN_DNS, ia5);
     sk_GENERAL_NAME_push(gens, gen);
     X509_add1_ext_i2d(*crt, NID_subject_alt_name, gens, 0, X509V3_ADD_DEFAULT);
@@ -180,40 +209,34 @@ int sign_certificate(EVP_PKEY *ca_key, X509 *ca_crt, EVP_PKEY **key, X509 **crt,
     X509_REQ_free(req);
     GENERAL_NAMES_free(gens);
 
-    return 1;
+    return 0;
 error:
     X509_REQ_free(req);
-    GENERAL_NAMES_free(gens);
     EVP_PKEY_free(*key);
     X509_free(*crt);
 
-    return 0;
+    return -1;
 }
 
-int generate_certificate(char *root_ca_key_path, char *root_ca_crt_path,
-                         EVP_PKEY **key, X509 **crt, char *hostname) {
-
-    // Load ROOT-CA key and certificate
-    EVP_PKEY *root_ca_key = NULL;
-    X509 *root_ca_crt = NULL;
-    if (!load_root_ca_key_and_crt(root_ca_key_path, &root_ca_key,
-                                  root_ca_crt_path, &root_ca_crt)) {
-        fprintf(stderr,
-                "Failed to load the root certificate and/or the root key!\n");
-
-        return 0;
-    }
+/**
+ * Load the root certificate, create a new certificate for the website and signs
+ * it to be used in a SSL connection.
+ *
+ * @param root_ca -> Information about the root ca.
+ * @param key -> Created key.
+ * @param crt -> Created certificate.
+ * @param hostname -> Domain (ex: www.example.com).
+ * @return -> 0 if success, -1 otherwise.
+ */
+int generate_certificate(struct root_ca root_ca, EVP_PKEY **key, X509 **crt,
+                         const char *hostname) {
 
     // Generate signed key and certificate by the CA.
-    int ret = sign_certificate(root_ca_key, root_ca_crt, key, crt, hostname);
-    if (!ret) {
+    int ret = sign_certificate(root_ca.root_ca_key, root_ca.root_ca_crt, key, crt, hostname);
+    if (ret == -1) {
         fprintf(stderr, "(error) Failed to sign certificate!\n");
-        return 0;
+        return -1;
     }
 
-    /* Free stuff. */
-    EVP_PKEY_free(root_ca_key);
-    X509_free(root_ca_crt);
-
-    return 1;
+    return 0;
 }
--- src/cert/cert.h
+++ src/cert/cert.h
@@ -2,7 +2,14 @@
 
 #include <openssl/x509v3.h>
 
-int generate_certificate(char *root_ca_key_path, char *root_ca_crt_path,
-                         EVP_PKEY **key, X509 **crt, char *hostname);
+struct root_ca {
+    EVP_PKEY *root_ca_key;
+    X509 *root_ca_crt;
+};
+
+int load_root_ca_key_and_crt(struct root_ca *root_ca, char *key_location,
+                             char *cert_location, char *key_password);
+int generate_certificate(struct root_ca root_ca, EVP_PKEY **key, X509 **crt,
+                         const char *hostname);
 
 #endif // !CERT_H
